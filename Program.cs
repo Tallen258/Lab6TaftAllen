32814c5814c2b6d2f435c52a777cfcf4d28d7851
Tallen258 <112665786+Tallen258@users.noreply.github.com>
2022-10-20 22:51:01 -0600

    finial push

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int StackNoParams() {
+    int a= 5;
+    int b= 8;
+    int c= 11;
+    int d= 14;
+    int e = 17;
+    return a;
+}
+ StackNoParams();
+void StackWithParams(int a, int b, int c){
+
+}
+void StackWithHeap(int a, int b, int c){
+int[]g=new int [3]{1,2,3};
+int h =5;
+int i =6;
+}
+void StackOver(long a){
+   
+StackOver(687);
+}
+StackOver(154);
+
+Console.ReadKey();
